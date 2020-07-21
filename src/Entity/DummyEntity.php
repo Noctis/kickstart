babce46deec7eb707305c8f6211bec08108c5c82
Lucas Czejgis <lucas.czejgis@gmail.com>
2020-07-21 11:33:10 +0200

    Added some standard dummy repository & entity files

---
+++ src/Entity/DummyEntity.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+namespace App\Entity;
+
+final class DummyEntity implements DummyEntityInterface
+{
+    /** @var int */
+    private $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getID(): int
+    {
+        return $this->id;
+    }
+}
