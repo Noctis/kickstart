34b66a47975d238b76962230b28b6c11f3817ff4
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-06-15 11:30:05 +0200

    `AbstractMiddleware` class is now explicitly a PSR-15 middleware.

--- src/Http/Middleware/AbstractMiddleware.php
+++ src/Http/Middleware/AbstractMiddleware.php
@@ -7,9 +7,10 @@ namespace Noctis\KickStart\Http\Middleware;
 use Noctis\KickStart\Http\Response\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-abstract class AbstractMiddleware
+abstract class AbstractMiddleware implements MiddlewareInterface
 {
     protected ResponseFactoryInterface $responseFactory;
 
