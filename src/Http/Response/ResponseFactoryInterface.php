e42292a30915f858a3253bd2d541666ba614b0c4
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-08-23 11:36:59 +0200

    Removed deprecated `AbstractAction::sendFile()` method (et consortes).

--- src/Http/Response/ResponseFactoryInterface.php
+++ src/Http/Response/ResponseFactoryInterface.php
@@ -8,7 +8,6 @@ use Laminas\Diactoros\Response\EmptyResponse;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Laminas\Diactoros\Response\RedirectResponse;
 use Noctis\KickStart\Http\Response\Attachment\AttachmentInterface;
-use Noctis\KickStart\File\FileInterface;
 use Psr\Http\Message\UriInterface;
 
 interface ResponseFactoryInterface
@@ -22,13 +21,6 @@ interface ResponseFactoryInterface
      * @param array<string, string> $params
      */
     public function redirectionResponse(UriInterface $uri, array $params = []): RedirectResponse;
-
-    /**
-     * @deprecated since version 2.1.0 (will be removed in 3.0.0)
-     * @see ResponseFactoryInterface::attachmentResponse()
-     * @psalm-suppress DeprecatedClass
-     */
-    public function fileResponse(FileInterface $file): FileResponse;
 
     public function attachmentResponse(AttachmentInterface $attachment): AttachmentResponse;
 
