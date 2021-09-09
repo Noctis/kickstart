0ad0dbe0a44bdc6ca654b0d61bdd93c870db0284
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-09-09 15:53:51 +0200

    A command loader factory can now be defined for console applications.
    See: https://symfony.com/doc/5.2/console/lazy_commands.html

--- src/Console/AbstractConsoleApplication.php
+++ src/Console/AbstractConsoleApplication.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace Noctis\KickStart\Console;
 
+use DI\Container;
 use Noctis\KickStart\AbstractApplication;
 use Noctis\KickStart\Console\Command\AbstractCommand;
 use Noctis\KickStart\Provider\ConfigurationProvider;
+use ReflectionProperty;
 use Symfony\Component\Console\Application as SymfonyConsoleApplication;
+use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
+use Symfony\Component\Console\CommandLoader\ContainerCommandLoader;
 
 abstract class AbstractConsoleApplication extends AbstractApplication
 {
-    /** @var list<class-string<AbstractCommand>> */
-    private array $commandsClassesNames;
+    /** @var callable */
+    private $commandLoaderFactory;
 
     /**
      * @param list<class-string<AbstractCommand>> $commandsClassesNames
@@ -21,15 +25,53 @@ abstract class AbstractConsoleApplication extends AbstractApplication
     {
         parent::__construct();
 
-        $this->commandsClassesNames = $commandsClassesNames;
+        $this->commandLoaderFactory =
+            function (Container $container) use ($commandsClassesNames): CommandLoaderInterface {
+                return new ContainerCommandLoader(
+                    $container,
+                    $this->buildCommandMap($commandsClassesNames)
+                );
+            };
     }
 
     public function run(): void
     {
         $app = new SymfonyConsoleApplication();
-        $this->registerCommands($app);
-
+        $app->setCommandLoader(
+            $this->getCommandLoader()
+        );
         $app->run();
+    }
+
+    public function setCommandLoaderFactory(callable $factory): void
+    {
+        $this->commandLoaderFactory = $factory;
+    }
+
+    private function getCommandLoader(): CommandLoaderInterface
+    {
+        /** @var CommandLoaderInterface */
+        return call_user_func($this->commandLoaderFactory, $this->container);
+    }
+
+    /**
+     * @param list<class-string<AbstractCommand>> $commandsClassesNames
+     *
+     * @return array<string, class-string<AbstractCommand>>
+     */
+    private function buildCommandMap(array $commandsClassesNames): array
+    {
+        $commandMap = [];
+        foreach ($commandsClassesNames as $className) {
+            $reflection = new ReflectionProperty($className, 'defaultName');
+            $reflection->setAccessible(true);
+            /** @var string $name */
+            $name = $reflection->getValue();
+
+            $commandMap[$name] = $className;
+        }
+
+        return $commandMap;
     }
 
     /**
@@ -40,16 +82,5 @@ abstract class AbstractConsoleApplication extends AbstractApplication
         return [
             new ConfigurationProvider(),
         ];
-    }
-
-    private function registerCommands(SymfonyConsoleApplication $app): void
-    {
-        foreach ($this->commandsClassesNames as $className) {
-            /** @var AbstractCommand $command */
-            $command = $this->container
-                ->get($className);
-
-            $app->add($command);
-        }
     }
 }
