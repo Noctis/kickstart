8596ad8c6f47b099f881dc558e36114d1b45b1a6
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-09-13 10:38:03 +0200

    Session manager can now be fetched from the `Request` object.

--- src/Http/Request/Request.php
+++ src/Http/Request/Request.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace Noctis\KickStart\Http\Request;
 
+use Laminas\Session\ManagerInterface;
+use Laminas\Session\SessionManager;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UploadedFileInterface;
 
 class Request
 {
     protected ServerRequestInterface $request;
+    protected ManagerInterface $sessionManager;
 
-    public function __construct(ServerRequestInterface $request)
+    public function __construct(ServerRequestInterface $request, SessionManager $sessionManager)
     {
         $this->request = $request;
+        $this->sessionManager = $sessionManager;
     }
 
     public function get(string $key, mixed $default = null): mixed
@@ -50,6 +54,13 @@ class Request
     {
         return $this->request
             ->getUploadedFiles();
+    }
+
+    public function getSessionID(): string
+    {
+        /** @var string */
+        return $this->sessionManager
+            ->getId();
     }
 
     public function getBaseHref(): string
