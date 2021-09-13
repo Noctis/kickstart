Noctis/kickstart

commit 871a80a1df41273ff6f58475c160d849282a1003

Session manager can now be fetched from the `Request` object.

src/Http/Request/Request.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace Noctis\KickStart\Http\Request;
 
+use Laminas\Session\ManagerInterface;
+use Laminas\Session\SessionManager;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UploadedFileInterface;
 
 class Request
 {
     protected ServerRequestInterface $request;
+    protected ManagerInterface $sessionManager;
 
-    public function __construct(ServerRequestInterface $request)
+    public function __construct(ServerRequestInterface $request, SessionManager $sessionManager)
     {
         $this->request = $request;
+        $this->sessionManager = $sessionManager;
     }
 
     public function get(string $key, mixed $default = null): mixed
@@ -50,6 +54,13 @@ class Request
     {
         return $this->request
             ->getUploadedFiles();
+    }
+
+    public function getSessionID(): string
+    {
+        /** @var string */
+        return $this->sessionManager
+            ->getId();
     }
 
     public function getBaseHref(): string

src/Provider/HttpServicesProvider.php
@@ -7,6 +7,7 @@ namespace Noctis\KickStart\Provider;
 use Laminas\Diactoros\UriFactory;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Laminas\Session\Container as SessionContainer;
 use Noctis\KickStart\Http\Factory\RequestFactory;
 use Noctis\KickStart\Http\Response\ResponseFactory;
 use Noctis\KickStart\Http\Response\ResponseFactoryInterface;
@@ -23,6 +24,7 @@ use Noctis\KickStart\Http\Routing\RoutesLoaderInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriFactoryInterface;
 
+use function DI\autowire;
 use function DI\factory;
 
 final class HttpServicesProvider implements ServicesProviderInterface
@@ -41,6 +43,8 @@ final class HttpServicesProvider implements ServicesProviderInterface
             ResponseFactoryInterface::class => ResponseFactory::class,
             RoutesLoaderInterface::class => RoutesLoader::class,
             ServerRequestInterface::class => factory([RequestFactory::class, 'createFromGlobals']),
+            SessionContainer::class => autowire(SessionContainer::class)
+                ->constructorParameter('name', 'flash'),
             UriFactoryInterface::class => UriFactory::class,
         ];
     }

commit 6366f8d83c01094359ae2ab6e9e2518d793f9749

Fixed BC break introduced in 871a80a1df41273ff6f58475c160d849282a1003.

src/Http/Request/Request.php
@@ -4,20 +4,16 @@ declare(strict_types=1);
 
 namespace Noctis\KickStart\Http\Request;
 
-use Laminas\Session\ManagerInterface;
-use Laminas\Session\SessionManager;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UploadedFileInterface;
 
 class Request
 {
     protected ServerRequestInterface $request;
-    protected ManagerInterface $sessionManager;
 
-    public function __construct(ServerRequestInterface $request, SessionManager $sessionManager)
+    public function __construct(ServerRequestInterface $request)
     {
         $this->request = $request;
-        $this->sessionManager = $sessionManager;
     }
 
     public function get(string $key, mixed $default = null): mixed
@@ -54,13 +50,6 @@ class Request
     {
         return $this->request
             ->getUploadedFiles();
-    }
-
-    public function getSessionID(): string
-    {
-        /** @var string */
-        return $this->sessionManager
-            ->getId();
     }
 
     public function getBaseHref(): string

src/Provider/HttpServicesProvider.php
@@ -8,6 +8,8 @@ use Laminas\Diactoros\UriFactory;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Laminas\Session\Container as SessionContainer;
+use Laminas\Session\ManagerInterface;
+use Laminas\Session\SessionManager;
 use Noctis\KickStart\Http\Factory\BaseHrefFactory;
 use Noctis\KickStart\Http\Factory\BaseHrefFactoryInterface;
 use Noctis\KickStart\Http\Factory\RequestFactory;
@@ -55,6 +57,7 @@ final class HttpServicesProvider implements ServicesProviderInterface
             FoundHandlerInterface::class => FoundHandler::class,
             HtmlResponseFactoryInterface::class => HtmlResponseFactory::class,
             HttpInfoProviderInterface::class => HttpInfoProvider::class,
+            ManagerInterface::class => SessionManager::class,
             MethodNotAllowedHandlerInterface::class => MethodNotAllowedHandler::class,
             NotFoundHandlerInterface::class => NotFoundHandler::class,
             NotFoundResponseFactoryInterface::class => NotFoundResponseFactory::class,