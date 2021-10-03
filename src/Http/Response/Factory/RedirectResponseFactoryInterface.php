669335d04c0a9a3b8c58ec29d2174c80030ec0cd
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-10-03 16:20:56 +0200

    Updated some doc-blocks.

--- src/Http/Response/Factory/RedirectResponseFactoryInterface.php
+++ src/Http/Response/Factory/RedirectResponseFactoryInterface.php
@@ -8,5 +8,8 @@ use Laminas\Diactoros\Response\RedirectResponse;
 
 interface RedirectResponseFactoryInterface
 {
+    /**
+     * @param array<string, string> $queryParams
+     */
     public function toPath(string $path, array $queryParams = []): RedirectResponse;
 }
