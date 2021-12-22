9500a4a8ee9d6326d671c7b33f17613adc1f6416
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-12-22 14:04:43 +0100

    Corrected namespace for `MiddlewareStackHandler` tests.

--- tests/acceptance/Http/Routing/MiddlewareStackHandler/HandleTests.php
+++ tests/acceptance/Http/Routing/MiddlewareStackHandler/HandleTests.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Acceptance\Http\Routing\RequestHandler;
+namespace Tests\Acceptance\Http\Routing\MiddlewareStackHandler;
 
 use DI\Container;
 use DI\ContainerBuilder;
