fff9f1675a047033509528759e9387612953ec0f
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-12-29 19:17:06 +0100

    Added a common (settable) DIC interface.

--- src/Provider/StandardServicesProvider.php
+++ src/Provider/StandardServicesProvider.php
@@ -7,8 +7,11 @@ namespace Noctis\KickStart\Provider;
 use Laminas\Diactoros\StreamFactory;
 use Noctis\KickStart\Http\Response\Attachment\AttachmentFactory;
 use Noctis\KickStart\Http\Response\Attachment\AttachmentFactoryInterface;
+use Noctis\KickStart\Service\Container\PhpDiContainer;
+use Noctis\KickStart\Service\Container\SettableContainerInterface;
 use Noctis\KickStart\Service\TemplateRendererInterface;
 use Noctis\KickStart\Service\TwigRenderer;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 
 final class StandardServicesProvider implements ServicesProviderInterface
@@ -20,6 +23,8 @@ final class StandardServicesProvider implements ServicesProviderInterface
     {
         return [
             AttachmentFactoryInterface::class => AttachmentFactory::class,
+            ContainerInterface::class => PhpDiContainer::class,
+            SettableContainerInterface::class => PhpDiContainer::class,
             StreamFactoryInterface::class => StreamFactory::class,
             TemplateRendererInterface::class  => TwigRenderer::class,
         ];
