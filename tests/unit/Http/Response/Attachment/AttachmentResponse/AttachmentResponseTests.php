c92ff4a0973be4d7414ca20002af7226be1330f3
Lucas Czejgis <lucas.czejgis@gmail.com>
2022-12-20 11:20:26 +0100

    Added tests for `AttachmentResponse`'s constructor

---
+++ tests/unit/Http/Response/Attachment/AttachmentResponse/AttachmentResponseTests.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Http\Response\Attachment\AttachmentResponse;
+
+use Fig\Http\Message\StatusCodeInterface;
+use Noctis\KickStart\Http\Response\Attachment\AttachmentInterface;
+use Noctis\KickStart\Http\Response\AttachmentResponse;
+use Noctis\KickStart\Http\Response\Headers\DispositionInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psr\Http\Message\StreamInterface;
+
+final class AttachmentResponseTests extends TestCase
+{
+    use ProphecyTrait;
+
+    public function test_it_sets_its_headers_based_on_given_attachment(): void
+    {
+        $attachment = $this->getAttachment('foo/bar', 'attachment');
+        $expectedResult = [
+            'Content-Encoding'    => ['none'],
+            'Content-Description' => ['File Transfer'],
+            'Content-Type'        => ['foo/bar'],
+            'Content-Disposition' => ['attachment'],
+        ];
+
+        $result = (new AttachmentResponse($attachment))
+            ->getHeaders();
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function test_standard_headers_overwrite_given_headers_if_there_is_a_conflict(): void
+    {
+        // `Content-Type: text/plain` should be overridden by attachment's `Content-Type: foo/bar`
+        // `X-Foo: bar` should stay as-is
+        $headers = [
+            'Content-Type' => 'text/plain',
+            'X-Foo'        => 'bar',
+        ];
+        $attachment = $this->getAttachment('foo/bar', 'attachment');
+        $expectedResult = [
+            'Content-Encoding'    => ['none'],
+            'Content-Description' => ['File Transfer'],
+            'Content-Type'        => ['foo/bar'],
+            'Content-Disposition' => ['attachment'],
+            'X-Foo'               => ['bar'],
+        ];
+
+        $result = (new AttachmentResponse($attachment, headers: $headers))
+            ->getHeaders();
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function test_200_ok_status_code_is_used_by_default_in_returned_response(): void
+    {
+        $attachment = $this->getAttachment('foo/bar', 'attachment');
+        $expectedResult = StatusCodeInterface::STATUS_OK;
+
+        $result = (new AttachmentResponse($attachment))
+            ->getStatusCode();
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    public function test_given_status_code_is_used_in_returned_response(): void
+    {
+        $attachment = $this->getAttachment('foo/bar', 'attachment');
+        $expectedResult = StatusCodeInterface::STATUS_SERVICE_UNAVAILABLE;
+
+        $result = (new AttachmentResponse($attachment, StatusCodeInterface::STATUS_SERVICE_UNAVAILABLE))
+            ->getStatusCode();
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    private function getAttachment(string $mimeType, string $disposition): AttachmentInterface
+    {
+        /** @var AttachmentInterface $attachment */
+        $attachment = $this->prophesize(AttachmentInterface::class);
+
+        $attachment->getStream()
+            ->willReturn(
+                $this->getStream()
+            );
+        $attachment->getMimeType()
+            ->willReturn($mimeType);
+
+        $attachment->getDisposition()
+            ->willReturn(
+                $this->getDisposition($disposition)
+            );
+
+        /** @var ObjectProphecy $attachment */
+        return $attachment->reveal();
+    }
+
+    private function getDisposition(string $value): DispositionInterface
+    {
+        /** @var DispositionInterface $disposition */
+        $disposition = $this->prophesize(DispositionInterface::class);
+
+        $disposition->toString()
+            ->willReturn($value);
+
+        /** @var ObjectProphecy $disposition */
+        return $disposition->reveal();
+    }
+
+    private function getStream(): StreamInterface
+    {
+        return $this->prophesize(StreamInterface::class)
+            ->reveal();
+    }
+}
