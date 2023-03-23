fcd5dd86b229ccfb2f6309d2993ebc2eaf5bcae9
Lucas Czejgis <lucas.czejgis@gmail.com>
2023-03-23 09:26:55 +0100

    Moved `TemplateRenderer` DI def. to a more appropriate service provider

--- src/Provider/StandardServicesProvider.php
+++ src/Provider/StandardServicesProvider.php
@@ -7,16 +7,14 @@ namespace Noctis\KickStart\Provider;
 use Laminas\Diactoros\StreamFactory;
 use Noctis\KickStart\Http\Response\Attachment\AttachmentFactory;
 use Noctis\KickStart\Http\Response\Attachment\AttachmentFactoryInterface;
-use Noctis\KickStart\Service\PathGenerator;
-use Noctis\KickStart\Service\PathGeneratorInterface;
-use Noctis\KickStart\Service\UrlGenerator;
-use Noctis\KickStart\Service\UrlGeneratorInterface;
 use Noctis\KickStart\Service\Container\DefinitionNormalizerInterface;
 use Noctis\KickStart\Service\Container\PhpDi\Container;
 use Noctis\KickStart\Service\Container\PhpDi\DefinitionNormalizer;
 use Noctis\KickStart\Service\Container\SettableContainerInterface;
-use Noctis\KickStart\Service\TemplateRendererInterface;
-use Noctis\KickStart\Service\TwigRenderer;
+use Noctis\KickStart\Service\PathGenerator;
+use Noctis\KickStart\Service\PathGeneratorInterface;
+use Noctis\KickStart\Service\UrlGenerator;
+use Noctis\KickStart\Service\UrlGeneratorInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 
@@ -41,7 +39,6 @@ final class StandardServicesProvider implements ServicesProviderInterface
                 ),
             SettableContainerInterface::class => Container::class,
             StreamFactoryInterface::class => StreamFactory::class,
-            TemplateRendererInterface::class  => TwigRenderer::class,
             UrlGeneratorInterface::class => UrlGenerator::class,
         ];
     }
