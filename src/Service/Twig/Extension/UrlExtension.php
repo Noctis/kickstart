c86e52be1d6185283b3185b670bfc24f74225840
Lucas Czejgis <lucas.czejgis@gmail.com>
2023-04-20 13:31:02 +0200

    Alternative: remove leading `/` from generated route (if not root)

--- src/Service/Twig/Extension/UrlExtension.php
+++ src/Service/Twig/Extension/UrlExtension.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Noctis\KickStart\Service\Twig\Extension;
 
-use Noctis\KickStart\Configuration\Configuration;
 use Noctis\KickStart\Service\PathGeneratorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+
 use function Psl\Regex\replace;
 
 final class UrlExtension extends AbstractExtension
@@ -32,10 +32,14 @@ final class UrlExtension extends AbstractExtension
      */
     private function getRoute(string $routeName, array $params = []): string
     {
-        $path = Configuration::getBaseHref() . $this->pathGenerator
+        $path = $this->pathGenerator
             ->toRoute($routeName, $params)
             ->toString();
 
-        return replace($path, '/\/\//', '');
+        if ($path !== '/') {
+            $path = replace($path, '/^\/+/', '');
+        }
+
+        return $path;
     }
 }
